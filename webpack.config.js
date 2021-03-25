8b8a9b03d5047914bf173179f180a980a5cc32ff
Cesar Ciello <cesarciello15@gmail.com>
2021-03-25 14:51:44 -0300

    feat: add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+module.exports = {
+  node: 'development',
+  entry: './src/main/index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'public/js'),
+    publicPath: '/public/js',
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extentions: ['.ts', '.tsx', '.js'],
+    alias: {
+        '@': path.join(__dirname, 'src')
+    }
+  },
+  devServer: {
+    contentBase: './public',
+    writeToDisk: true,
+    historyApiFallback: true
+  },
+  externals: {
+    react: 'React',
+    'react-dom': 'ReactDOM'
+  },
+  plugins: [
+    new CleanWebpackPlugin()
+  ]
+}
