90d6ba3646a93add2f24c4da1fd89de4bd339d91
Cesar Ciello <cesarciello15@gmail.com>
2021-03-26 20:29:30 -0300

    test: ensure EmailValidation return falsy if no email is provided

--- src/validation/validators/email/email-validation.spec.ts
+++ src/validation/validators/email/email-validation.spec.ts
@@ -3,7 +3,7 @@ import faker from 'faker'
 import { EmailValidation } from './email-validation'
 import { InavlidFieldError } from '@/validation/error'
 
-const makeSut = (): EmailValidation => new EmailValidation()
+const makeSut = (): EmailValidation => new EmailValidation('email')
 
 describe('EmailValidation', () => {
   test('should return error if email as invalid', () => {
@@ -15,6 +15,12 @@ describe('EmailValidation', () => {
   test('should return falsy on success', () => {
     const sut = makeSut()
     const error = sut.validate(faker.internet.email())
+    expect(error).toBeFalsy()
+  })
+
+  test('should return falsy if no email is provided', () => {
+    const sut = makeSut()
+    const error = sut.validate('')
     expect(error).toBeFalsy()
   })
 })
