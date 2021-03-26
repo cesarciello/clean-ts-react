143a7652929d712585f74d3ae7fe41825289ccd5
Cesar Ciello <cesarciello15@gmail.com>
2021-03-26 20:05:47 -0300

    test: ensure EmailValidation return falsy on success

--- src/validation/validators/email/email-validation.spec.ts
+++ src/validation/validators/email/email-validation.spec.ts
@@ -1,3 +1,5 @@
+import faker from 'faker'
+
 import { EmailValidation } from './email-validation'
 import { InavlidFieldError } from '@/validation/error'
 
@@ -6,7 +8,13 @@ const makeSut = (): EmailValidation => new EmailValidation()
 describe('EmailValidation', () => {
   test('should return error if email as invalid', () => {
     const sut = makeSut()
-    const error = sut.validate('')
+    const error = sut.validate(faker.random.word())
     expect(error).toEqual(new InavlidFieldError('email'))
+  })
+
+  test('should return falsy on success', () => {
+    const sut = makeSut()
+    const error = sut.validate(faker.internet.email())
+    expect(error).toBeFalsy()
   })
 })
