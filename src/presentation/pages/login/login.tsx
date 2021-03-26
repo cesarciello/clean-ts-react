01b96545d803351c95933caab38aec691ebbba8f
Cesar Ciello <cesarciello15@gmail.com>
2021-03-26 07:36:29 -0300

    fix: label error

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -8,7 +8,7 @@ const Login: React.FC = () => {
       <LoginHeader />
       <form action="" className={Styles.form}>
         <h2>Login</h2>
-        <Input type="email" name="email" placeholder="Digite seu emial" />
+        <Input type="email" name="email" placeholder="Digite seu email" />
         <Input type="password" name="password" placeholder="Digite sua senha" />
         <button type="submit">Entrar</button>
         <span className={Styles.link}>criar Conta</span>
