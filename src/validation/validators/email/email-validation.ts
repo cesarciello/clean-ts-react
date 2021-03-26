baa3d7a69ba68ddecd166456f7d1f6fc6d1ed6df
Cesar Ciello <cesarciello15@gmail.com>
2021-03-26 20:29:20 -0300

    feat: ensure EmailValidation return falsy if no email is provided

--- src/validation/validators/email/email-validation.ts
+++ src/validation/validators/email/email-validation.ts
@@ -6,7 +6,7 @@ export class EmailValidation implements FieldValidation {
 
   validate(value: string): Error {
     const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-    if (emailRegex.test(value)) {
+    if (emailRegex.test(value) || value === '') {
       return null
     }
     return new InavlidFieldError('email')
