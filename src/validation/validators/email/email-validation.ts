a0e2ffd79ba2a4555a2537bb79022d9ce485943d
Cesar Ciello <cesarciello15@gmail.com>
2021-03-26 20:03:51 -0300

    feat: ensure EmailValidation return falsy on success

--- src/validation/validators/email/email-validation.ts
+++ src/validation/validators/email/email-validation.ts
@@ -1,10 +1,14 @@
-import { InavlidFieldError } from '@/validation/error';
+import { InavlidFieldError } from '@/validation/error'
 import { FieldValidation } from '@/validation/protocols/field-validation'
 
 export class EmailValidation implements FieldValidation {
   field: string;
 
   validate(value: string): Error {
+    const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    if (emailRegex.test(value)) {
+      return null
+    }
     return new InavlidFieldError('email')
   }
 }
