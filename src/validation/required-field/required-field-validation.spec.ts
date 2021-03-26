2fd31f19d2dd01521c50b014bd4cfe3459a68257
Cesar Ciello <cesarciello15@gmail.com>
2021-03-26 19:15:05 -0300

    chore: move sut creation to a method

--- src/validation/required-field/required-field-validation.spec.ts
+++ src/validation/required-field/required-field-validation.spec.ts
@@ -3,15 +3,17 @@ import faker from 'faker'
 import { RequiredFieldError } from '@/validation/error'
 import { RequiredFieldValidation } from '@/validation/required-field/required-field-validation'
 
+const makeSut = (): RequiredFieldValidation => new RequiredFieldValidation('field')
+
 describe('RequiredFieldValidation', () => {
   test('should return error if field is empty', () => {
-    const sut = new RequiredFieldValidation('field')
+    const sut = makeSut()
     const error = sut.validate('')
     expect(error).toEqual(new RequiredFieldError('field'))
   })
 
   test('should return falsy if field is not empty', () => {
-    const sut = new RequiredFieldValidation('field')
+    const sut = makeSut()
     const error = sut.validate(faker.random.word())
     expect(error).toBeFalsy()
   })
