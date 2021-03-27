a2ef785554d0f744946e9a1474ea10b0d98d2b20
Cesar Ciello <cesarciello15@gmail.com>
2021-03-27 18:58:42 -0300

    feat: ensure SingUp start with correct initial state

--- src/presentation/pages/singup/singup.tsx
+++ src/presentation/pages/singup/singup.tsx
@@ -1,21 +1,33 @@
 import { Link } from 'react-router-dom'
-import React from 'react'
+import React, { useState } from 'react'
 
 import Styles from './singup-styles.scss'
 import Context from '@/presentation/context/form-login/form-login-context'
 import { Footer, LoginHeader, Input, FormStatusLogin } from '@/presentation/components'
 
 const SingUp: React.FC = () => {
+  const [state] = useState({
+    isLoading: false,
+    errorMessage: '',
+    name: '',
+    nameError: '',
+    email: '',
+    emailError: '',
+    password: '',
+    passwordError: '',
+    passwordConfirmation: '',
+    passwordConfirmationError: ''
+  })
   return (
     <div className={Styles.singup}>
       <LoginHeader />
-      <Context.Provider value={{ state: {} }}>
+      <Context.Provider value={{ state }}>
         <form data-testid="form" className={Styles.form}>
           <h2>SingUp</h2>
           <Input type="text" name="name" placeholder="Digite seu nome" />
           <Input type="email" name="email" placeholder="Digite seu email" />
           <Input type="password" name="password" placeholder="Digite sua senha" />
-          <Input type="password" name="passwordConfirmarion" placeholder="Confirme sua senha" />
+          <Input type="password" name="passwordConfirmation" placeholder="Confirme sua senha" />
           <button disabled className={Styles.submit} data-testid="submit" type="submit">cadastrar</button>
           <Link to="/login" data-testid="singup" className={Styles.link}>Voltar parar o Login</Link>
           <FormStatusLogin />
