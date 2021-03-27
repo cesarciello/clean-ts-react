10b89cf8676cb63c3ab54964835a9c369b2a2df8
Cesar Ciello <cesarciello15@gmail.com>
2021-03-27 17:54:11 -0300

    test: ensure CompareFieldsValidation return null if compare is valid

--- src/validation/validators/compare-fields/compare-fields-validation.spec.ts
+++ src/validation/validators/compare-fields/compare-fields-validation.spec.ts
@@ -2,11 +2,19 @@ import { InavlidFieldError } from '@/validation/error'
 import faker from 'faker'
 import { CompareFieldsValidation } from './compare-fields-validation'
 
-const makeSut = (valueToCompare: string): CompareFieldsValidation => (new CompareFieldsValidation(faker.database.column(), valueToCompare))
+const makeSut = (fieldToCompare, valueToCompare: string): CompareFieldsValidation => (new CompareFieldsValidation(fieldToCompare, valueToCompare))
 describe('CompareFieldsValidation', () => {
   test('should return error if compare is invalid', () => {
-    const sut = makeSut(faker.random.word())
+    const fieldToCompare = faker.database.column()
+    const sut = makeSut(fieldToCompare, faker.random.word())
     const error = sut.validate(faker.random.word())
-    expect(error).toEqual(new InavlidFieldError(''))
+    expect(error).toEqual(new InavlidFieldError(fieldToCompare))
+  })
+
+  test('should return null if compare is valid', () => {
+    const value = faker.random.word()
+    const sut = makeSut(faker.database.column(), value)
+    const error = sut.validate(value)
+    expect(error).toBeFalsy()
   })
 })
