f1cee138e939524255f19614290b7e3fdc05b279
Cesar Ciello <cesarciello15@gmail.com>
2021-03-27 17:29:11 -0300

    feat: ensure RemoteAddAccount throw UnexpectedError if HttpPostClient returns 500

--- src/data/usecases/add-account/remote-add-account.ts
+++ src/data/usecases/add-account/remote-add-account.ts
@@ -1,5 +1,5 @@
 import { HttpPostClient, HttpStatusCode } from '@/data/protocols/http'
-import { InvalidCredentialsError } from '@/domain/errors'
+import { InvalidCredentialsError, UnexpectedError } from '@/domain/errors'
 import { AddAccount } from '@/domain/usecases/add-account'
 
 export class RemoteAddAccount implements AddAccount {
@@ -16,6 +16,7 @@ export class RemoteAddAccount implements AddAccount {
     switch (httpResponse.statusCode) {
       case HttpStatusCode.success: return httpResponse.body
       case HttpStatusCode.unauthorized: throw new InvalidCredentialsError()
+      default: throw new UnexpectedError()
     }
   }
 }
