4abaf80b0f472264562602fb2f17d509f94903a3
Cesar Ciello <cesarciello15@gmail.com>
2021-03-27 08:14:08 -0300

    test: ensure ValidationBuilder return EmailValidation

--- src/validation/validators/builder/validation-builder.spec.ts
+++ src/validation/validators/builder/validation-builder.spec.ts
@@ -1,9 +1,14 @@
 import { ValidationBuilder as sut } from './validation-builder'
-import { RequiredFieldValidation } from '@/validation/validators'
+import { RequiredFieldValidation, EmailValidation } from '@/validation/validators'
 
 describe('ValidationBuilder', () => {
-  test('should return required field validation', () => {
+  test('should return RequiredFieldValidation', () => {
     const validators = sut.field('any_field').required().build()
     expect(validators).toEqual([new RequiredFieldValidation('any_field')])
+  })
+
+  test('should return EmailValidation', () => {
+    const validators = sut.field('any_field').email().build()
+    expect(validators).toEqual([new EmailValidation('any_field')])
   })
 })
