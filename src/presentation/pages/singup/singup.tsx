ff80c05e1362bd95d41758c67ee02bc5688a362f
Cesar Ciello <cesarciello15@gmail.com>
2021-03-28 08:46:47 -0300

    feat: ensure SinUp page show Password error if validation fails

--- src/presentation/pages/singup/singup.tsx
+++ src/presentation/pages/singup/singup.tsx
@@ -28,9 +28,10 @@ const SingUp: React.FC<Props> = ({ validation }: Props) => {
     setState({
       ...state,
       nameError: validation.validate('name', state.name),
-      emailError: validation.validate('email', state.email)
+      emailError: validation.validate('email', state.email),
+      passwordError: validation.validate('password', state.password)
     })
-  }, [state.name, state.email])
+  }, [state.name, state.email, state.password])
 
   return (
     <div className={Styles.singup}>
