4ee6d59e76b12ecdf1dde884a674304803f30829
Cesar Ciello <cesarciello15@gmail.com>
2021-03-28 08:48:13 -0300

    feat: ensure SinUp page show Password Confirmation error if validation fails

--- src/presentation/pages/singup/singup.tsx
+++ src/presentation/pages/singup/singup.tsx
@@ -29,9 +29,10 @@ const SingUp: React.FC<Props> = ({ validation }: Props) => {
       ...state,
       nameError: validation.validate('name', state.name),
       emailError: validation.validate('email', state.email),
-      passwordError: validation.validate('password', state.password)
+      passwordError: validation.validate('password', state.password),
+      passwordConfirmationError: validation.validate('passwordConfirmation', state.passwordConfirmation)
     })
-  }, [state.name, state.email, state.password])
+  }, [state.name, state.email, state.password, state.passwordConfirmation])
 
   return (
     <div className={Styles.singup}>
