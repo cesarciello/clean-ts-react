7f4fb55a703a010f8ee041cb3d4e429856d21e61
Cesar Ciello <cesarciello15@gmail.com>
2021-03-28 19:27:54 -0300

    test:  ensure Login present valid if form is valid

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -15,7 +15,15 @@ describe('Login', () => {
     cy.getByTestId('email').type(faker.random.word())
     cy.getByTestId('email-error').should('have.text', 'Invalid field email')
     cy.getByTestId('password').type(faker.random.alphaNumeric(4))
-    cy.getByTestId('submit').should('have.attr', 'disabled')
     cy.getByTestId('password-error').should('have.text', 'Min Length field is 5')
+    cy.getByTestId('submit').should('have.attr', 'disabled')
+  })
+
+  it('should present valid state if form is valid', () => {
+    cy.getByTestId('email').type(faker.internet.email())
+    cy.getByTestId('email-error').should('not.exist')
+    cy.getByTestId('password').type(faker.internet.password())
+    cy.getByTestId('password-error').should('not.exist')
+    cy.getByTestId('submit').should('not.have.attr', 'disabled')
   })
 })
