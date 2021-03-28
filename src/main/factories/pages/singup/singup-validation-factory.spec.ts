ad75c88a3908188d5e5194a89ffb1b8431890a0b
Cesar Ciello <cesarciello15@gmail.com>
2021-03-28 14:57:29 -0300

    test: add SingUpValidationFactory

---
+++ src/main/factories/pages/singup/singup-validation-factory.spec.ts
@@ -0,0 +1,14 @@
+import { ValidationBuilder, ValidationComposite } from '@/validation/validators'
+import { makeSingUpValidationFactory } from './singup-validation-factory'
+
+describe('SingUpValidationFactory', () => {
+  test('should compose Validation with correct validators', () => {
+    const composite = makeSingUpValidationFactory()
+    expect(composite).toEqual(new ValidationComposite([
+      ...ValidationBuilder.field('name').required().min(10).build(),
+      ...ValidationBuilder.field('email').required().email().build(),
+      ...ValidationBuilder.field('password').required().min(5).build(),
+      ...ValidationBuilder.field('passwordConfirmation').required().compare('password').min(10).build()
+    ]))
+  })
+})
