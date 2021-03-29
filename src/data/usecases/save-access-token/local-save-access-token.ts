438d4e44f248aa18a06ba76e05c90eac3e1e45bf
Cesar Ciello <cesarciello15@gmail.com>
2021-03-29 10:48:35 -0300

    feat: ensure LocalSaveAccessToken throws if undefined is provided

--- src/data/usecases/save-access-token/local-save-access-token.ts
+++ src/data/usecases/save-access-token/local-save-access-token.ts
@@ -1,10 +1,14 @@
-import { SetStorage } from '@/data/protocols/storage/set-storage'
+import { UnexpectedError } from '@/domain/errors'
 import { SaveAccessToken } from '@/domain/usecases/save-acess-token'
+import { SetStorage } from '@/data/protocols/storage/set-storage'
 
 export class LocalSaveAccessToken implements SaveAccessToken {
   constructor(private readonly setStorage: SetStorage) { }
 
   async save(accessToken: string): Promise<void> {
+    if (!accessToken) {
+      throw new UnexpectedError()
+    }
     await this.setStorage.set('accessToken', accessToken)
   }
 }
