1f5845497a8d11d903eb8a3a262bfa1f3b361843
Cesar Ciello <cesarciello15@gmail.com>
2021-03-30 09:52:44 -0300

    refactor: rename vars

--- src/infra/http/axios-http-client/axios-http-client.ts
+++ src/infra/http/axios-http-client/axios-http-client.ts
@@ -4,23 +4,23 @@ import { HttpGetClient } from '@/data/protocols/http/http-get-client'
 
 export class AxiosHttpClient<R> implements HttpPostClient<R>, HttpGetClient<R> {
   async post(params: HttpPostClient.Params): Promise<HttpPostClient.Result<R>> {
-    let httpResponse: AxiosResponse
+    let axiosResponse: AxiosResponse
     try {
-      httpResponse = await axios.post(params.url, params.body)
+      axiosResponse = await axios.post(params.url, params.body)
     } catch (error) {
-      httpResponse = error.response
+      axiosResponse = error.response
     }
     return {
-      statusCode: httpResponse.status,
-      body: httpResponse.data
+      statusCode: axiosResponse.status,
+      body: axiosResponse.data
     }
   }
 
   async get(params: HttpGetClient.Params): Promise<HttpGetClient.Result<R>> {
-    const httpReponse = await axios.get(params.url)
+    const axiosResponse = await axios.get(params.url)
     return {
-      statusCode: httpReponse.status,
-      body: httpReponse.data
+      statusCode: axiosResponse.status,
+      body: axiosResponse.data
     }
   }
 }
