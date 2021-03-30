c3e631d7f478ff4ddee3385d4b9f47f478eea58e
Cesar Ciello <cesarciello15@gmail.com>
2021-03-30 09:51:57 -0300

    feat: ensure AxiosHttpClient.get return correct result

--- src/infra/http/axios-http-client/axios-http-client.ts
+++ src/infra/http/axios-http-client/axios-http-client.ts
@@ -1,7 +1,8 @@
 import axios, { AxiosResponse } from 'axios'
 import { HttpPostClient } from '@/data/protocols/http'
+import { HttpGetClient } from '@/data/protocols/http/http-get-client'
 
-export class AxiosHttpClient<R> implements HttpPostClient<R> {
+export class AxiosHttpClient<R> implements HttpPostClient<R>, HttpGetClient<R> {
   async post(params: HttpPostClient.Params): Promise<HttpPostClient.Result<R>> {
     let httpResponse: AxiosResponse
     try {
@@ -12,6 +13,14 @@ export class AxiosHttpClient<R> implements HttpPostClient<R> {
     return {
       statusCode: httpResponse.status,
       body: httpResponse.data
+    }
+  }
+
+  async get(params: HttpGetClient.Params): Promise<HttpGetClient.Result<R>> {
+    const httpReponse = await axios.get(params.url)
+    return {
+      statusCode: httpReponse.status,
+      body: httpReponse.data
     }
   }
 }
