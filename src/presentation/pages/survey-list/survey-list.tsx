eef5309f00b793c6272148b84d6f252311b6d5cb
Cesar Ciello <cesarciello15@gmail.com>
2021-03-31 14:58:41 -0300

    feat: ensure SurveyList call LoadSurveyList

--- src/presentation/pages/survey-list/survey-list.tsx
+++ src/presentation/pages/survey-list/survey-list.tsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 import Styles from './survey-list-styles.scss'
 import { Footer, Header } from '@/presentation/components'
 import { SurveyItemEmpty, SurveyItem } from './components'
+import { LoadSurveyList } from '@/domain/usecases/load-survey-list'
 
-const SurveyList: React.FC = () => {
+type Props = {
+  loadSurveyList: LoadSurveyList
+}
+
+const SurveyList: React.FC<Props> = ({ loadSurveyList }: Props) => {
+  useEffect(() => {
+    (async function () {
+      await loadSurveyList.loadAll()
+    })()
+  }, [])
+
   return (
     <div data-testid="survey-list" className={Styles.surveyListWrap}>
       <Header />
