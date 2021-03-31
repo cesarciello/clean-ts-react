db280bc2da621bd23ddc6d0badae98b9403fbb67
Cesar Ciello <cesarciello15@gmail.com>
2021-03-31 10:24:19 -0300

    test: ensure LocalStorageAdpter calls localStorage.getItem with correct values

--- src/infra/storage/local-storage-adpater.spec.ts
+++ src/infra/storage/local-storage-adpater.spec.ts
@@ -18,11 +18,21 @@ describe('LocalStorageAdpter', () => {
     localStorage.clear()
   })
 
-  test('should calls localStorage with correct values', async () => {
+  test('should calls localStorage.setItem with correct values', async () => {
     const { sut } = makeSut()
     const key = faker.database.column()
     const value = faker.random.objectElement<{ name: string }>()
     sut.set(key, value)
     expect(localStorage.setItem).toHaveBeenLastCalledWith(key, JSON.stringify(value))
+  })
+
+  test('should calls localStorage.getItem with correct values', async () => {
+    const { sut } = makeSut()
+    const key = faker.database.column()
+    const value = faker.random.objectElement<{ name: string }>()
+    const getItemSpy = jest.spyOn(localStorage, 'getItem').mockReturnValueOnce(JSON.stringify(value))
+    const storageResponse = sut.get(key)
+    expect(storageResponse).toEqual(value)
+    expect(getItemSpy).toHaveBeenLastCalledWith(key)
   })
 })
