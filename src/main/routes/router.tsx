3d1001e202dce26101d517bb66c7e773948f07ae
Cesar Ciello <cesarciello15@gmail.com>
2021-03-31 11:45:38 -0300

    feat: ensure PrivateRoute ender current component if token is not empty

--- src/main/routes/router.tsx
+++ src/main/routes/router.tsx
@@ -3,13 +3,16 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 import { SurveyList } from '@/presentation/pages'
 import ApiContext from '@/presentation/context/api/api-context'
-import { setCurrentAccountAdpter } from '@/main/adpters/current-account-adpter'
+import { setCurrentAccountAdpter, getCurrentAccountAdpter } from '@/main/adpters/current-account-adpter'
 import { makeLoginPageFactory } from '@/main/factories/pages/login/login-factory'
 import { makeSignUpPageFactory } from '@/main/factories/pages/signup/signup-factory'
 
 const Router: React.FC = () => {
   return (
-    <ApiContext.Provider value={{ setCurrentAccount: setCurrentAccountAdpter }}>
+    <ApiContext.Provider value={{
+      setCurrentAccount: setCurrentAccountAdpter,
+      getCurrentAccount: getCurrentAccountAdpter
+    }}>
       <BrowserRouter>
         <Switch>
           <Route path="/login" exact component={makeLoginPageFactory} />
---
+++ src/presentation/components/private-route/private-route.tsx
@@ -0,0 +1,12 @@
+import React, { useContext } from 'react'
+import { Redirect, Route, RouteProps } from 'react-router'
+import ApiContext from '../../context/api/api-context'
+
+const PrivateRoute: React.FC<RouteProps> = (props: RouteProps) => {
+  const { getCurrentAccount } = useContext(ApiContext)
+  return getCurrentAccount()?.accessToken
+    ? <Route {...props} />
+    : <Route {...props} component={() => <Redirect to="/login" />} />
+}
+
+export default PrivateRoute
