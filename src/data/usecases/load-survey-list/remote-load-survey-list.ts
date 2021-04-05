54d8ec957368784a116a8c71350ca284d43025cc
Cesar Ciello <cesarciello15@gmail.com>
2021-04-05 17:06:42 -0300

    feat: ensure RemoteLoadSurveyList throws AccessDeniedError on 403

--- src/data/usecases/load-survey-list/remote-load-survey-list.ts
+++ src/data/usecases/load-survey-list/remote-load-survey-list.ts
@@ -1,6 +1,6 @@
 import { LoadSurveyList } from '@/domain/usecases/load-survey-list'
 import { HttpGetClient } from '@/data/protocols/http/http-get-client'
-import { UnexpectedError } from '@/domain/errors'
+import { AccessDeniedError, UnexpectedError } from '@/domain/errors'
 import { HttpStatusCode } from '@/data/protocols/http'
 
 export class RemoteLoadSurveyList implements LoadSurveyList {
@@ -15,6 +15,7 @@ export class RemoteLoadSurveyList implements LoadSurveyList {
     switch (httpResponse.statusCode) {
       case (HttpStatusCode.success): return remoteSurvey.map(remoteSurvey => Object.assign(remoteSurvey, { date: new Date(remoteSurvey.date) }))
       case (HttpStatusCode.noContent): return []
+      case (HttpStatusCode.forbidden): throw new AccessDeniedError()
       default: throw new UnexpectedError()
     }
   }
