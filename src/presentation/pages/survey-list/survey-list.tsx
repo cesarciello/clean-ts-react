f7cb255f3f9f9be582cb20e95867c2784653a2bc
Cesar Ciello <cesarciello15@gmail.com>
2021-04-05 09:22:04 -0300

    feat: ensure SurveyList render SurveyItem on success and error message on failure

--- src/presentation/pages/survey-list/survey-list.tsx
+++ src/presentation/pages/survey-list/survey-list.tsx
@@ -1,8 +1,9 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 import Styles from './survey-list-styles.scss'
-import { Footer, Header } from '@/presentation/components'
 import { SurveyItemEmpty, SurveyItem } from './components'
+import { Footer, Header } from '@/presentation/components'
+import { SurveyModel } from '@/domain/models/survey-model'
 import { LoadSurveyList } from '@/domain/usecases/load-survey-list'
 
 type Props = {
@@ -10,20 +11,40 @@ type Props = {
 }
 
 const SurveyList: React.FC<Props> = ({ loadSurveyList }: Props) => {
+  const [state, setState] = useState({
+    surveys: [] as SurveyModel[],
+    error: ''
+  })
+
   useEffect(() => {
-    (async function () {
-      await loadSurveyList.loadAll()
-    })()
+    loadSurveyList.loadAll()
+      .then((surveys) => setState({ ...state, surveys }))
+      .catch((error) => setState({ ...state, error: error.message }))
   }, [])
 
   return (
-    <div data-testid="survey-list" className={Styles.surveyListWrap}>
+    <div className={Styles.surveyListWrap}>
       <Header />
       <section className={Styles.contentWarp}>
         <h2>Enquete</h2>
-        <ul>
-          <SurveyItemEmpty />
-        </ul>
+        {
+          state.error
+            ? (
+              <div>
+                <span data-testid="error">
+                  {state.error}
+                </span>
+                <button>recarregar</button>
+              </div>)
+            : (
+              <ul data-testid="survey-list">
+                {
+                  state.surveys.length
+                    ? state.surveys.map((survey: SurveyModel) => <SurveyItem key={survey.id} survey={survey} />)
+                    : <SurveyItemEmpty />
+                }
+              </ul>)
+        }
       </section>
       <Footer />
     </div>
