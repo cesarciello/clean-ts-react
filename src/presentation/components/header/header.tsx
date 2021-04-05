c288b789d09bd494255ca1217b2bacc739428d2b
Cesar Ciello <cesarciello15@gmail.com>
2021-04-05 16:31:52 -0300

    feat: ensure logout calls setCurrentAccount with null

--- src/presentation/components/header/header.tsx
+++ src/presentation/components/header/header.tsx
@@ -1,15 +1,25 @@
-import React, { memo } from 'react'
+import React, { memo, useContext } from 'react'
+
+import ApiContext from '@/presentation/context/api/api-context'
 import Logo from '../logo/logo'
 import Styles from './header-styles.scss'
+import { useHistory } from 'react-router'
 
 const Header: React.FC = () => {
+  const history = useHistory()
+  const { setCurrentAccount } = useContext(ApiContext)
+  const logout = (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>): void => {
+    event.preventDefault()
+    setCurrentAccount(null)
+    history.replace('/login')
+  }
   return (
     <header className={Styles.headerWrap}>
       <div className={Styles.headerContent}>
         <Logo />
         <div className={Styles.logoutWrap}>
           <span>Cesar</span>
-          <a href="#">Sair</a>
+          <a data-testid="logout" href="#" onClick={logout}>Sair</a>
         </div>
       </div>
     </header>
