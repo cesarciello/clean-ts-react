4fdc89529eab7889a310b48fb3dd873768d96561
Cesar Ciello <cesarciello15@gmail.com>
2021-04-05 15:49:14 -0300

    feat: ensure LocalStorageAdpter calls localStorage.removeItem with value is null

--- src/main/adapters/current-account-adapter.ts
+++ src/main/adapters/current-account-adapter.ts
@@ -1,11 +1,7 @@
-import { UnexpectedError } from '@/domain/errors'
 import { UpdateCurrentAccount } from '@/domain/usecases/update-current-account'
 import { makeLocalStorageAdapterFactory } from '@/main/factories/storage/local-storage-adapter-factory'
 
 export const setCurrentAccountAdapter = (account: UpdateCurrentAccount.Params): void => {
-  if (!account?.accessToken) {
-    throw new UnexpectedError()
-  }
   makeLocalStorageAdapterFactory().set('account', account)
 }
 
