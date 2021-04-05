aefce377358f8376a962b5dab99036143eafff21
Cesar Ciello <cesarciello15@gmail.com>
2021-04-05 12:21:33 -0300

    feat: ensure AuthorizedHttpGetClientDecorator not add token headers if get storage is invalid

--- src/main/decorators/authorized-http-get-client-decorator/authorized-http-get-client-decorator.ts
+++ src/main/decorators/authorized-http-get-client-decorator/authorized-http-get-client-decorator.ts
@@ -3,11 +3,13 @@ import { GetStorage } from '@/data/protocols/storage/get-storage'
 
 export class AuthorizedHttpGetClientDecorator implements HttpGetClient<any> {
   constructor(
-    private readonly getStorage: GetStorage
+    private readonly getStorage: GetStorage,
+    private readonly httpGetClient: HttpGetClient<any>
   ) { }
 
   async get(params: HttpGetClient.Params): Promise<HttpGetClient.Result<any>> {
     this.getStorage.get('account')
+    await this.httpGetClient.get(params)
     return null
   }
 }
