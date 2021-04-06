5f442b3c174b052ea9bdba3948ea976639e35255
Cesar Ciello <cesarciello15@gmail.com>
2021-04-06 11:41:56 -0300

    chore: add fixture account

--- src/main/test/cypress/utils/http-mocks.ts
+++ src/main/test/cypress/utils/http-mocks.ts
@@ -3,10 +3,10 @@ import faker from 'faker'
 export const mockInvalidCredentialsRequest = (url: RegExp): void => (mockHttpRequest(url, 401, { error: faker.random.word() }))
 export const mockForbbidenRequest = (url: RegExp): void => (mockHttpRequest(url, 403, { error: faker.random.word() }))
 export const mockUnexpectedRequest = (url: RegExp): void => (mockHttpRequest(url, faker.helpers.randomize([400, 404, 500]), { error: faker.random.word() }))
-export const mockLoginRequest = (url: RegExp): void => (mockHttpRequest(url, 200, { accessToken: faker.random.uuid(), name: faker.name.findName() }))
+export const mockLoginRequest = (url: RegExp): void => (mockHttpRequest(url, 200, 'fx:account'))
 export const mockLoginNoDataRequest = (url: RegExp): void => (mockHttpRequest(url, 200))
 
-const mockHttpRequest = (url: RegExp, statusCode: number, body?: object): void => {
+const mockHttpRequest = (url: RegExp, statusCode: number, body?: any): void => {
   cy.intercept(url, (req) => {
     req.reply((res) => {
       res.send({ statusCode, body })
