d55702fb4f286472d7f62dba3c401414e080216f
Cesar Ciello <cesarciello15@gmail.com>
2021-04-06 14:31:12 -0300

    chore: add node serve

--- webpack.common.js
+++ webpack.common.js
@@ -7,7 +7,6 @@ module.exports = {
   entry: './src/main/index.tsx',
   output: {
     path: path.join(__dirname, 'dist'),
-    publicPath: '/dist',
     filename: 'main-bundle-[fullhash].js'
   },
   resolve: {
