0329c64a67bf47d2878b43aac72a7463f066fdc6
Cesar Ciello <cesarciello15@gmail.com>
2021-04-07 13:53:17 -0300

    feat: add skeleton shimmer in SurveyResult

--- src/presentation/pages/survey-result/survey-result.tsx
+++ src/presentation/pages/survey-result/survey-result.tsx
@@ -9,28 +9,22 @@ const Surveyresult: React.FC = () => {
     <div className={Styles.surveyResultWrap}>
       <Header />
       <section className={Styles.contentWarp}>
-        <hgroup>
-          <Calendar date={new Date()} className={Styles.calendarWarp} />
-          <h2>Question ?</h2>
-        </hgroup>
-        <FlipMove className={Styles.answerList}>
-          <li>
-            <img src="http://fordevs.herokuapp.com/static/img/logo-react.png" />
-            <span className={Styles.answer}>Answer</span>
-            <span className={Styles.percent}>100%</span>
-          </li>
-          <li className={Styles.active}>
-            <img src="http://fordevs.herokuapp.com/static/img/logo-react.png" />
-            <span className={Styles.answer}>Answer</span>
-            <span className={Styles.percent}>100%</span>
-          </li>
-          <li>
-            <img src="http://fordevs.herokuapp.com/static/img/logo-react.png" />
-            <span className={Styles.answer}>Answer</span>
-            <span className={Styles.percent}>100%</span>
-          </li>
-        </FlipMove>
-        <button>back</button>
+        {false &&
+          <>
+            <hgroup>
+              <Calendar date={new Date()} className={Styles.calendarWarp} />
+              <h2>Lorem, ipsum dolor dolores consequuntur ipsam ullam saepe nulla ?</h2>
+            </hgroup>
+            <FlipMove className={Styles.answerList}>
+              <li>
+                <img src="http://fordevs.herokuapp.com/static/img/logo-react.png" />
+                <span className={Styles.answer}>Answer</span>
+                <span className={Styles.percent}>100%</span>
+              </li>
+            </FlipMove>
+            <button>back</button>
+          </>
+        }
         {false && <Loading />}
       </section>
       <Footer />
