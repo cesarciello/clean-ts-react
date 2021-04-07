01bb2fffafa053f2322cee4bb143a1f51efde24b
Cesar Ciello <cesarciello15@gmail.com>
2021-04-07 08:56:33 -0300

    fix: test compare fields remove faker

--- src/validation/validators/compare-fields/compare-fields-validation.spec.ts
+++ src/validation/validators/compare-fields/compare-fields-validation.spec.ts
@@ -1,18 +1,17 @@
-import { InavlidFieldError } from '@/validation/error'
 import faker from 'faker'
+
+import { InavlidFieldError } from '@/validation/error'
 import { CompareFieldsValidation } from './compare-fields-validation'
 
 const makeSut = (field, fieldToCompare: string): CompareFieldsValidation => (new CompareFieldsValidation(field, fieldToCompare))
 describe('CompareFieldsValidation', () => {
   test('should return error if compare is invalid', () => {
-    const field = faker.database.column()
-    const fieldToCompare = faker.database.column()
-    const sut = makeSut(field, fieldToCompare)
+    const sut = makeSut('field', 'fieldToCompare')
     const error = sut.validate({
-      [field]: faker.datatype.number(6),
-      [fieldToCompare]: faker.random.words(4)
+      field: 'any_value',
+      fieldToCompare: 'other_value'
     })
-    expect(error).toEqual(new InavlidFieldError(field))
+    expect(error).toEqual(new InavlidFieldError('field'))
   })
 
   test('should return null if compare is valid', () => {
