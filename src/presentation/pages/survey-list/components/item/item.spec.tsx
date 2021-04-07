3f3d0634cbab5ac816efbea37fc42a2e140863f6
Cesar Ciello <cesarciello15@gmail.com>
2021-04-07 14:20:51 -0300

    fix: change calendar test to item SurveyList

--- src/presentation/pages/survey-list/components/item/item.spec.tsx
+++ src/presentation/pages/survey-list/components/item/item.spec.tsx
@@ -21,20 +21,28 @@ describe('SurveyItem', () => {
   test('should render with correct values', () => {
     const survey = {
       ...mockSurvey(),
-      didAnswer: true
+      didAnswer: true,
+      date: new Date('2021-03-30T00:00:00')
     }
     makeSut(survey)
     expect(screen.getByTestId('icon')).toHaveProperty('src', IconName.thumbUp)
     expect(screen.getByTestId('question')).toHaveTextContent(survey.question)
+    expect(screen.getByTestId('day')).toHaveTextContent('30')
+    expect(screen.getByTestId('month')).toHaveTextContent('mar')
+    expect(screen.getByTestId('year')).toHaveTextContent('2021')
   })
 
   test('should render with correct load icon and day correcty', () => {
     const survey = {
       ...mockSurvey(),
-      didAnswer: false
+      didAnswer: false,
+      date: new Date('2021-03-03T00:00:00')
     }
     makeSut(survey)
     expect(screen.getByTestId('icon')).toHaveProperty('src', IconName.thumbDown)
     expect(screen.getByTestId('question')).toHaveTextContent(survey.question)
+    expect(screen.getByTestId('day')).toHaveTextContent('03')
+    expect(screen.getByTestId('month')).toHaveTextContent('mar')
+    expect(screen.getByTestId('year')).toHaveTextContent('2021')
   })
 })
