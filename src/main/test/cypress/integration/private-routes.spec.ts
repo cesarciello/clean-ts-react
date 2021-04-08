ee5d8ab0125da21c48f3b139238ef91905be9de3
Cesar Ciello <cesarciello15@gmail.com>
2021-04-08 13:46:00 -0300

    test: ensure logout if survey-result has no token

--- src/main/test/cypress/integration/private-routes.spec.ts
+++ src/main/test/cypress/integration/private-routes.spec.ts
@@ -5,4 +5,9 @@ describe('Private Routes', () => {
     cy.visit('')
     cy.url().should('eq', `${baseUrl}/login`)
   })
+
+  it('should logout if survey-result has no token', () => {
+    cy.visit('/surveys/any_id/results')
+    cy.url().should('eq', `${baseUrl}/login`)
+  })
 })
