4b533aa986fb286e5da03a06f579b1766a63b4c1
Cesar Ciello <cesarciello15@gmail.com>
2021-04-08 08:52:14 -0300

    feat: ensure SurveyResult present SurveyResult on success

--- src/presentation/pages/survey-result/survey-result.tsx
+++ src/presentation/pages/survey-result/survey-result.tsx
@@ -4,38 +4,50 @@ import FlipMove from 'react-flip-move'
 import Styles from './survey-result-styles.scss'
 import { LoadSurveyResult } from '@/domain/usecases/load-survey-result'
 import { Calendar, Footer, Header, Loading, Error } from '@/presentation/components'
+import { LoadSurveyResultSpy } from '@/domain/test'
 
 type Props = {
   loadSurveyResult: LoadSurveyResult
 }
 
-const Surveyresult: React.FC<Props> = ({ loadSurveyResult }: Props) => {
-  const [state] = useState({
+const Surveyresult: React.FC<Props> = ({ loadSurveyResult = new LoadSurveyResultSpy() }: Props) => {
+  const [state, setState] = useState({
     isLoading: false,
     error: '',
-    surveyresult: null as LoadSurveyResult.Result
+    surveyResult: null as LoadSurveyResult.Result
   })
 
   useEffect(() => {
     loadSurveyResult.load()
+      .then(surveyResult => {
+        setState(old => ({
+          ...old,
+          surveyResult
+        })
+        )
+      })
   }, [])
 
   return (
     <div className={Styles.surveyResultWrap}>
       <Header />
       <section data-testid="survey-result" className={Styles.contentWarp}>
-        {state.surveyresult &&
+        {state.surveyResult &&
           <>
             <hgroup>
-              <Calendar date={new Date()} className={Styles.calendarWarp} />
-              <h2>Lorem, ipsum dolor dolores consequuntur ipsam ullam saepe nulla ?</h2>
+              <Calendar date={state.surveyResult.date} className={Styles.calendarWarp} />
+              <h2 data-testid="question">{state.surveyResult.question}</h2>
             </hgroup>
-            <FlipMove className={Styles.answerList}>
-              <li>
-                <img src="http://fordevs.herokuapp.com/static/img/logo-react.png" />
-                <span className={Styles.answer}>Answer</span>
-                <span className={Styles.percent}>100%</span>
-              </li>
+            <FlipMove data-testid="answers" className={Styles.answerList}>
+              {
+                state.surveyResult.answers.map(answer => (
+                  <li key={answer.answer} data-testid="answer-wrap" className={answer.isCurrentAccountAnswer ? Styles.active : ''} >
+                    {answer.image && <img data-testid="image" src={answer.image} alt={answer.answer} />}
+                    <span data-testid="answer" className={Styles.answer}>{answer.answer}</span>
+                    <span data-testid="percent" className={Styles.percent}>{answer.percent}%</span>
+                  </li>
+                ))
+              }
             </FlipMove>
             <button>back</button>
           </>
