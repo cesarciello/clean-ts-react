4a35f91a88aab06252a55994f546a7974801a385
Cesar Ciello <cesarciello15@gmail.com>
2021-04-09 15:25:41 -0300

    test: ensure RemoteSaveSurveyResult throws AccessDeniedError if httpClient returns 403

--- src/data/usecases/save-survey-result/remote-save-survey-result.spec.ts
+++ src/data/usecases/save-survey-result/remote-save-survey-result.spec.ts
@@ -3,12 +3,17 @@ import faker from 'faker'
 import { HttpClientSpy } from '@/data/test'
 import { RemoteSaveSurveyResult } from './remote-save-survey-result'
 import { mockRemoteSurveyResult } from '@/domain/test/mock-survey-result'
+import { HttpStatusCode } from '@/data/protocols/http'
+import { AccessDeniedError } from '@/domain/errors'
 
 type SutTypes = {
   sut: RemoteSaveSurveyResult
   httpClientSpy: HttpClientSpy<any>
 }
 
+const answerToSave = {
+  answer: faker.random.words()
+}
 const url = faker.internet.url()
 
 const makeSut = (): SutTypes => {
@@ -24,10 +29,17 @@ const makeSut = (): SutTypes => {
 describe('RemoteSaveSurveyResult', () => {
   test('should call httpClient with correct url and method', async () => {
     const { sut, httpClientSpy } = makeSut()
-    await sut.save({
-      answer: faker.random.words()
-    })
+    await sut.save(answerToSave)
     expect(httpClientSpy.url).toBe(url)
     expect(httpClientSpy.method).toBe('put')
+  })
+
+  test('should throws AccessDeniedError if httpClient returns 403', async () => {
+    const { sut, httpClientSpy } = makeSut()
+    httpClientSpy.httpResponse = {
+      statusCode: HttpStatusCode.forbidden
+    }
+    const promise = sut.save(answerToSave)
+    await expect(promise).rejects.toThrow(new AccessDeniedError())
   })
 })
